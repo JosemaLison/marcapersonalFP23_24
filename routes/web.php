a221d7b1cac8c71d085b312024c2ea42610fb6e7
Jose Manuel Lisón <1173569@alu.murciaeduca.es>
2023-11-14 08:49:11 +0100

    Hasta la incorporación del navbar

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Pantalla principal";
+    return view('home');
 });
 
 Route::get('/home/{nombre}/{apellidos?}', function($nombre, $apellidos = null)
